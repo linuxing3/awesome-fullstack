d3419a3edbeff1821f009f7f9417668804f8c15c
linuxing3 <linuxing3@qq.com>
2019-01-25 22:02:41 -0400

    777

---
+++ client/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  lintOnSave: true,
+  configureWebpack: config => {
+    changeTarget(config);
+  }
+};
+
+function changeTarget(config) {
+  config.target = "electron-renderer";
+  return config;
+};
